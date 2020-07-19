823d0bb674022d0c34269b3e5fc9ddcb36857c6d
PealMazumder <peal6614@gmail.com>
2020-07-19 13:48:30 +0600

    initial commit

---
+++ AtCoder/AtCoder Beginner Contest 172/D - Sum of Divisors.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define nl "\n"
+using namespace std;
+void solve()
+{
+	int n;
+	scanf("%d",&n);
+	
+	vector<int> div(n+1, 0);
+	for(int i = 1; i<=n; i++)
+	{
+		for(int j = 0; j<=n; j += i)
+			div[j]++;
+	}
+	ll ans = 0;
+	for(ll i = 1; i<=n; i++)
+		ans += (i * (ll)div[i]);
+	
+	printf("%lld\n",ans);
+}
+int main()
+{
+	solve();
+	return 0;
+}

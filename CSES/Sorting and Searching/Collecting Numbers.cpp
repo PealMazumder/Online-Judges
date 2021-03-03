6adbf3451ec5488c731b132f0c6d1c9b73ddba40
PealMazumder <peal6614@gmail.com>
2021-03-03 14:53:43 +0600

    initial commit

---
+++ CSES/Sorting and Searching/Collecting Numbers.cpp
@@ -0,0 +1,36 @@
+//Problem Link: https://cses.fi/problemset/task/2216/
+#include<bits/stdc++.h>
+#define	ll	long long
+#define	pb	push_back
+#define	sz(v)	(int)v.size()
+#define	nl	"\n"
+using namespace std;
+
+void solve()
+{
+	int n, m, x;
+	cin>>n;
+	int ar[n];
+	vector<pair<int, int> > v;
+	for(int i = 0; i<n; i++)
+		cin>>ar[i], v.pb({ar[i], i});
+	sort(v.begin(), v.end());
+	int cnt = 1;
+	for(int i = 1; i<sz(v); i++)
+		if(v[i-1].second > v[i].second)cnt++;
+	cout<<cnt<<nl;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);cout.tie(NULL);
+
+//	int t;
+//	cin>>t;
+//	while(t--)
+	    solve();
+
+	return 0;
+}
+
+

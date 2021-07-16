5ab17fb23712bdd5e497ca820fe507d7968fa75f
Peal Mazumder <peal6614@gmail.com>
2021-07-17 01:17:37 +0600

    delete

--- LeetCode/Top Interview Questions/Missing Number.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int missingNum = nums.size();
-        for(int i = 0; i<(int) nums.size(); i++)
-            missingNum ^= (i ^ nums[i]);
-        return missingNum;
-    }
-};

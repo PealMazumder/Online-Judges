08c9ef084365890c0d99460134d881942e2d855b
Peal Mazumder <peal6614@gmail.com>
2021-10-04 10:54:23 +0600

    initial commit

---
+++ LeetCode/Top Interview Questions/EASY/169. Majority Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, majority;
+        
+        for(int num : nums) {
+            if(count == 0) majority = num;
+            count += (num == majority) ? 1 : -1;
+        }
+        return majority;
+    }
+};

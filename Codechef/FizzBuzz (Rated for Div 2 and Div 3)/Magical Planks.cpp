ad04c4d25d8b0b9ce40c2e27b01b668e9a34b455
Peal Mazumder <peal6614@gmail.com>
2021-12-05 00:16:42 +0600

    initial commit

---
+++ Codechef/FizzBuzz (Rated for Div 2 and Div 3)/Magical Planks.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#define		nl				"\n"
+#define		mod				1000000007
+#define		sz(c)			(int)c.size()
+#define		pb				push_back
+#define		ff				first
+#define		ss				second
+#define		mkp				make_pair
+#define		ll				long long
+#define		pii				pair<int, int>
+#define 	pll				pair<ll, ll>
+#define 	vll				vector<ll>
+#define 	vi				vector<int>
+#define		no				cout<<"NO\n"
+#define		yes				cout<<"YES\n"
+#define		one				cout<<"1\n"
+#define		mone			cout<<"-1\n"
+#define		gcd(a, b)		__gcd(a, b)
+#define		total_bit(a)	__builtin_popcount(a)
+#define		lcm(a, b)		((a)*((b)/gcd(a,b)))
+#define		REP(i,n)		for(int i = 0; i<n; i++)
+#define		all(p)			p.begin(),p.end()
+#define		mem(ar,val)		memset(ar, val, sizeof(ar))
+#define 	UNIQUE(a)		sort(all(a)); a.erase(unique(all(a)), a.end())
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const double pi = acos(-1.0);
+const ll INF = 1000000000000000000;
+const int N = 1e6+7;
+
+void solve()
+{
+	int n, i, b = 0, w = 0, cnt = 0;
+	cin>>n;
+	string s;
+	cin>>s;
+	s += "#";
+	for(i = 0; i<n; i++)
+    {
+        if(s[i] != s[i+1])
+        {
+            if(s[i] == 'B') b++; else w++;
+        }
+    }
+
+	cout<<min(w, b)<<"\n";
+
+}
+int main()
+{
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(0);cout.tie(NULL);
+	ll t;
+	cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
